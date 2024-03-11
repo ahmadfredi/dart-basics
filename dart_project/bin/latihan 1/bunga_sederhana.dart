b9cacb251eb8cbcc1ceeda6640c757d715eb13af
ahmadfredi <ahmadfredi02@gmail.com>
2024-03-11 18:50:25 +0700

    bunga sederhana

---
+++ dart_project/bin/latihan 1/bunga_sederhana.dart
@@ -0,0 +1,8 @@
+void main() {
+  const int p = 1000; // Principal
+  const int t = 2;    // Time in years
+  const int r = 5;    // Rate of interest
+
+  double bunga = (p * t * r) / 100;
+  print("Bunga sederhana adalah: $bunga");
+}
