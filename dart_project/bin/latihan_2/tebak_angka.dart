adb0c2475502580d751f08416e933eeb805727b2
ahmadfredi <ahmadfredi02@gmail.com>
2024-03-11 19:09:44 +0700

    tebak angka

---
+++ dart_project/bin/latihan_2/tebak_angka.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  Random random = new Random();
+  int angkaRandom = random.nextInt(100) + 1;
+  int tebakan;
+  bool tebakanBenar = false;
+
+  do {
+    stdout.write("Tebak angka (1-100): ");
+    tebakan = int.parse(stdin.readLineSync()!);
+
+    if (tebakan == angkaRandom) {
+      print("Selamat, tebakan Anda benar!");
+      tebakanBenar = true;
+    } else if (tebakan < angkaRandom) {
+      print("Tebakan terlalu kecil, coba lagi.");
+    } else {
+      print("Tebakan terlalu besar, coba lagi.");
+    }
+  } while (!tebakanBenar);
+}
